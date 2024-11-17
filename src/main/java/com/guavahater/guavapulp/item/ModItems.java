305b895daeac2c52104a19d16ee2070de631aef3
Zane Stockton <zane.stockton@hotmail.com>
2024-11-16 21:21:18 -0700

    Update ModItems.java

--- src/main/java/com/guavahater/guavapulp/item/ModItems.java
+++ src/main/java/com/guavahater/guavapulp/item/ModItems.java
@@ -10,6 +10,7 @@ public class ModItems
 {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(GuavaPulp.MOD_ID);
 
+    //Item registry for the incense item.
     public static final DeferredItem<Item> INCENSE = ITEMS.register
             ("incense", () -> new Item(new Item.Properties()));
 
