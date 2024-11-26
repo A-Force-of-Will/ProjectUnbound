fc3e6c546f068db27c68d02ae598ecd3790e4ba9
Zane Stockton <zane.stockton@hotmail.com>
2024-11-26 14:27:38 -0700

    Recipies and Loot Tables
    Added LT for sandalwood & glass bowl. Added crafting for glass bowl

--- src/main/java/com/guavahater/guavapulp/GuavaPulp.java
+++ src/main/java/com/guavahater/guavapulp/GuavaPulp.java
@@ -1,6 +1,7 @@
 package com.guavahater.guavapulp;
 
 import com.guavahater.guavapulp.block.ModBlocks;
+import com.guavahater.guavapulp.item.ModCreativeModeTabs;
 import com.guavahater.guavapulp.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -40,6 +41,8 @@ public class GuavaPulp
         // Note that this is necessary if and only if we want *this* class (GuavaPulp) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/com/guavahater/guavapulp/item/ModCreativeModeTabs.java
@@ -0,0 +1,42 @@
+package com.guavahater.guavapulp.item;
+
+import com.guavahater.guavapulp.GuavaPulp;
+import com.guavahater.guavapulp.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GuavaPulp.MOD_ID);
+    //ITEMS
+    public static final Supplier<CreativeModeTab> GUAVAPULP_ITEMS_TAB = CREATIVE_MODE_TAB.register("guavapulp_items_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.SMALL_LEAF.get()))
+            .title(Component.translatable("creativetab.guavapulp.guavapulp_items"))
+            .displayItems((itemDisplayparameters, output) -> {
+                output.accept(ModItems.SMALL_LEAF);
+                output.accept(ModItems.INCENSE);
+            }).build());
+
+    //BLOCKS
+    public static final Supplier<CreativeModeTab> GUAVAPULP_BLOCKS_TAB = CREATIVE_MODE_TAB.register("guavapulp_blocks_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModBlocks.GLASS_BOWL))
+            .withTabsBefore(ResourceLocation.fromNamespaceAndPath(GuavaPulp.MOD_ID, "guavapulp_items_tab"))
+            .title(Component.translatable("creativetab.guavapulp.guavapulp_blocks"))
+            .displayItems((itemDisplayparameters, output) -> {
+                output.accept(ModBlocks.GLASS_BOWL);
+                output.accept(ModBlocks.SANDALWOOD_LOG);
+            }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
+
+
